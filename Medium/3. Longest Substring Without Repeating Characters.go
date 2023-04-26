39adc6af05bfda5d5ce74e013d824aa216bb3ddd
Quang (Waang) <81631701+orgball2608@users.noreply.github.com>
2023-04-26 20:52:28 +0700

    sliding windown problem (medium)

---
+++ Medium/3. Longest Substring Without Repeating Characters.go
@@ -0,0 +1,20 @@
+func lengthOfLongestSubstring(s string) int {
+    dict := make(map[byte]int)
+    start:=0
+    length:=0
+    for i, _ := range s {
+        dict[s[i]]++
+        for dict[s[i]] > 1 {
+            if dict[s[start]] == 1 {
+                delete(dict,s[start])
+            } else {
+                dict[s[start]]--
+            }
+            start++
+        }
+        if length < (i-start+1){
+            length = (i-start+1)
+        }
+    }
+    return length
+}
---
+++ Medium/424. Longest Repeating Character Replacement.go
@@ -0,0 +1,18 @@
+func characterReplacement(s string, k int) int {
+    dict := make(map[byte]int)
+    start,maxlength,maxrepeatchar := 0,0,0
+    for i, _ := range s {
+        dict[s[i]]++
+        if maxrepeatchar <dict[s[i]] {
+            maxrepeatchar= dict[s[i]]
+        }
+        if (i-start+1-maxrepeatchar) > k {
+            dict[s[start]]--
+            start++
+        }
+        if maxlength < i-start+1 {
+            maxlength = i-start+1
+        }
+    }
+    return maxlength
+}

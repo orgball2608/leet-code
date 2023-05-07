bf7444223d00ef2e4735ef800adeb16eafa0192f
Quang (Waang) <81631701+orgball2608@users.noreply.github.com>
2023-05-07 23:57:15 +0700

    add

---
+++ Easy/202. Happy Number.go
@@ -0,0 +1,36 @@
+func isHappy(n int) bool {
+  slow := n
+  fast := sumDigits(n)
+
+  for fast != 1 && fast != slow {
+    slow = sumDigits(slow)
+    fast = sumDigits(sumDigits(fast))
+  }
+
+  if fast == 1 {
+    return true
+  }
+
+  return false
+}
+
+func pow(digit, power int) int {
+  r := 1
+
+  for i := 0; i < power;i++ {
+    r *= digit
+  }
+
+  return r
+}
+
+func sumDigits(number int) int {
+  sum := 0
+  for number > 0 {
+    digit := number % 10
+    number = number / 10
+    sum += pow(digit, 2)
+  }
+
+  return sum
+}

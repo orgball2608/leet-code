9fd2a00467238e89ff41cad3f152563083c16c2d
Huynh Minh Quang <quanghuynhwtf@gmail.com>
2023-10-06 23:43:40 +0700

    feat: solve sliding windows problem

---
+++ Medium/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.go
@@ -0,0 +1,14 @@
+func numOfSubarrays(arr []int, k int, threshold int) int {
+    l,sum,res:=0,0,0
+    for r, val := range arr {
+        sum += val
+        if r-l+1 >= k {
+            if sum/k>= threshold {
+                res++
+            }
+            sum -= arr[l]
+            l++
+        }
+    }
+    return res
+}

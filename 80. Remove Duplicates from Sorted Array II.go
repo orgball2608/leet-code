8fa855238df99be931fefc4fcb1fbaa96948d221
Huỳnh Minh Quang <quanghm@algolabs.vn>
2023-10-12 23:27:01 +0700

    feat: solve two pointer problem

---
+++ 80. Remove Duplicates from Sorted Array II.go
@@ -0,0 +1,13 @@
+func removeDuplicates(nums []int) int {
+    if len(nums) < 2 {
+        return len(nums)
+    }
+    next := 2
+    for i:=2; i < len(nums); i++ {
+        if(nums[next-2]!= nums[i]){
+            nums[next] = nums[i]
+            next++
+        }
+    }
+    return next
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # LeetCode
+
 Try hard leet code to improve level

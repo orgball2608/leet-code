a7899a44984af75a243e22e841527e88a1d96694
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-10-23 22:47:20 +0700

    feat: solve two pointer problem

---
+++ Easy/28. Find the Index of the First Occurrence in a String.go
@@ -0,0 +1,17 @@
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		match := true
+		for j := 0; j < len(needle); j++ {
+			if haystack[i+j] != needle[j] {
+				match = false
+			}
+		}
+		if match {
+			return i
+		}
+	}
+	return -1
+}

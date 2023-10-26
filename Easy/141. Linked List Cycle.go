dcc9d66f5dcf75e8dcbb1eeae2e2f85e669f2f8e
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2023-10-26 22:20:15 +0700

    feat: solve slow pointer, fast pointer problem

---
+++ Easy/141. Linked List Cycle.go
@@ -0,0 +1,12 @@
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}

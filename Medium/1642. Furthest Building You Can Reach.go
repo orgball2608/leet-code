d2b696b3f3212aa771d49ae7cbfa242ac6568a3a
Huynh Minh Quang <81631701+orgball2608@users.noreply.github.com>
2024-02-17 22:00:49 +0700

    feat: solve min heap problem

---
+++ Medium/1642. Furthest Building You Can Reach.go
@@ -0,0 +1,37 @@
+import (
+	"container/heap"
+	"fmt"
+)
+
+type MinHeap []int
+
+func (h MinHeap) Len() int            { return len(h) }
+func (h MinHeap) Less(i, j int) bool  { return h[i] < h[j] }
+func (h MinHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *MinHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *MinHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func furthestBuilding(heights []int, bricks int, ladders int) int {
+	minHeap := &MinHeap{}
+	for i := 1; i < len(heights); i++ {
+		diff := heights[i] - heights[i-1]
+		if diff < 0 {
+			continue
+		}
+		heap.Push(minHeap, diff)
+		if minHeap.Len() > ladders {
+			bricks -= heap.Pop(minHeap).(int)
+		}
+		if bricks < 0 {
+			return i - 1
+		}
+	}
+	return len(heights) - 1
+}
+

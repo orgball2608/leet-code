0d0202bf9f54838e25f1d087af7bf321053e4f24
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2024-07-07 23:39:58 +0700

    feat: solve problem 20

---
+++ Easy/20. Valid Parentheses.go
@@ -0,0 +1,29 @@
+package easy
+
+func isValid(s string) bool {
+	length := len(s)
+	if length == 0 || length%2 == 1 {
+		return false
+	}
+
+	pairs := map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	stack := make([]rune, length%2)
+	for _, r := range s {
+		if _, ok := pairs[r]; ok {
+			stack = append(stack, r)
+		} else if len(stack) == 0 || pairs[stack[len(stack)-1]] != r {
+			return false
+		} else {
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
+
+//Time: O(N)
+//Space: O(N/2)

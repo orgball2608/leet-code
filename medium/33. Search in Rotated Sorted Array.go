9b6f385dd091abf67fa75dc5007cbec58f32a404
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2024-08-02 12:39:13 +0700

    feat: solve problem 33

---
+++ medium/33. Search in Rotated Sorted Array.go
@@ -0,0 +1,30 @@
+package medium
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && nums[mid] > target {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && nums[right] >= target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+//Time: O(logN)
+//Space: O(1)

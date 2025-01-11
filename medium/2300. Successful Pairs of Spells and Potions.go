775c37fd703e3bf8e3d558c1eeb3ae50ab8e19a1
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-01-11 08:53:50 +0700

    feat: solve problem 2540

---
+++ medium/2300. Successful Pairs of Spells and Potions.go
@@ -0,0 +1,16 @@
+package medium
+
+func successfulPairs(spells []int, potions []int, success int64) []int {
+	slices.Sort(potions)
+	succ := int(success) - 1
+	pairs := make([]int, 0, len(spells))
+	for i := range spells {
+		atLeast := succ/spells[i] + 1
+		pos, _ := slices.BinarySearch(potions, atLeast)
+		pairs = append(pairs, len(potions)-pos)
+	}
+	return pairs
+}
+
+// Time: O(MlogM+NlogM)
+// Space: O(N)

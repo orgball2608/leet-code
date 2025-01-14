4fe9b93fce991164714d5cefb9e2ba6bece76e0c
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-01-14 22:54:37 +0700

    feat: solve problem 1338

---
+++ medium/1338. Reduce Array Size to The Half.go
@@ -0,0 +1,26 @@
+package medium
+
+func minSetSize(arr []int) int {
+	counter := make([]int, 100001)
+	for _, val := range arr {
+		counter[val]++
+	}
+
+	slices.Sort(counter)
+	slices.Reverse(counter)
+
+	removed, ans := 0, 0
+	half := len(arr) / 2
+	for _, val := range counter {
+		removed += val
+		ans++
+		if removed >= half {
+			break
+		}
+	}
+
+	return ans
+}
+
+// Time : O(N+100001log100001+N)
+// Space: O(100001)

73725ba908fa2123526977a4e3266e18f8602d7e
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-02-05 21:22:13 +0700

    feat: solve problem 389

--- easy/202. Happy Number.go
+++ easy/202. Happy Number.go
@@ -3,12 +3,10 @@ package easy
 func isHappy(n int) bool {
 	slow := n
 	fast := sumDigits(n)
-
-	for fast != 1 && fast != slow {
+	for fast != 0 && slow != fast {
 		slow = sumDigits(slow)
 		fast = sumDigits(sumDigits(fast))
 	}
-
 	if fast == 1 {
 		return true
 	}
@@ -16,23 +14,12 @@ func isHappy(n int) bool {
 	return false
 }
 
-func pow(digit, power int) int {
-	r := 1
-
-	for i := 0; i < power; i++ {
-		r *= digit
-	}
-
-	return r
-}
-
 func sumDigits(number int) int {
 	sum := 0
 	for number > 0 {
 		digit := number % 10
 		number = number / 10
-		sum += pow(digit, 2)
+		sum += digit * digit
 	}
-
 	return sum
 }

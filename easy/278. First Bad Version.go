c3b6da2f8643e3b46cb7b4eff40eb77d766c8edc
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-02-07 22:08:08 +0700

    feat: solve problem 278

---
+++ easy/278. First Bad Version.go
@@ -0,0 +1,26 @@
+package easy
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	lo, hi := 0, n
+	for lo < hi {
+		mid := lo + (hi-lo)/2
+		if isBadVersion(mid) {
+			hi = mid
+		} else {
+			lo = mid + 1
+		}
+	}
+
+	return lo
+}
+
+// Time: O(logN)
+// Space: O(1)

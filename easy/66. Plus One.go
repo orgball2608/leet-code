f72c91080c45c5fc57f6cf12fca8d6b31883de5c
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-02-19 23:04:52 +0700

    feat: solve problem 66

---
+++ easy/66. Plus One.go
@@ -0,0 +1,16 @@
+package easy
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+
+	return append([]int{1}, digits...)
+}
+
+// Time: O(N)
+// Space: O(N)

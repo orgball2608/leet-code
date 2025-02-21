d9d8edac4b2ee7158635c00019badea8396d330c
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-02-21 21:49:56 +0700

    feat: solve problem 48

---
+++ medium/48. Rotate Image.go
@@ -0,0 +1,17 @@
+package medium
+
+func rotate(matrix [][]int) {
+	n, m := len(matrix), len(matrix[0])
+	for i := 0; i < n; i++ {
+		for j := i; j < m; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+
+		for j, k := 0, m-1; j < k; j, k = j+1, k-1 {
+			matrix[i][j], matrix[i][k] = matrix[i][k], matrix[i][j]
+		}
+	}
+}
+
+// Time: O(N^2)
+// Space: O(N^2)

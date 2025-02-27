7709921b4b5227a3fb434616f4f8e96822195568
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-02-27 22:22:36 +0700

    feat: solve problem 867

---
+++ medium/876. Middle of the Linked List.go
@@ -0,0 +1,21 @@
+package medium
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	return slow
+}
+
+// Time: O(N)
+// Space: O(1)

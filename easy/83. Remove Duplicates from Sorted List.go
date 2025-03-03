b0c9714b0b91dc61e6b566414136430561959928
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-03-03 22:24:34 +0700

    feat: solve problem 83

---
+++ easy/83. Remove Duplicates from Sorted List.go
@@ -0,0 +1,23 @@
+package easy
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	temp := head
+	for temp != nil && temp.Next != nil {
+		if temp.Val == temp.Next.Val {
+			temp.Next = temp.Next.Next
+		} else {
+			temp = temp.Next
+		}
+	}
+	return head
+}
+
+// Time: O(N)
+// Space: O(1)

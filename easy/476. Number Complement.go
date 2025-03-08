d926ef663a61666247ac0dfff073dcd1ca4d592d
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-03-08 17:29:31 +0700

    feat: solve problem 476

---
+++ easy/476. Number Complement.go
@@ -0,0 +1,32 @@
+package easy
+
+func findComplement(num int) int {
+	temp := num
+	count := 0
+	for temp > 0 {
+		num = swap(num, count)
+		count++
+		temp = temp >> 1
+	}
+	return num
+}
+
+func swap(u int, k int) int {
+	return u ^ (1 << k)
+}
+
+// O(logN)
+// Space: O(1)
+
+func findComplement(num int) int {
+	temp := num
+	length := 0
+	for temp > 0 {
+		length++
+		temp = temp >> 1
+	}
+	return ((1 << length) - 1) ^ num
+}
+
+// O(logN)
+// Space: O(1)

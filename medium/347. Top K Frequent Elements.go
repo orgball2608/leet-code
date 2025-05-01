fdfb1d402a10a7964a6027796185ea1b5f152d2e
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-05-01 23:32:03 +0700

    feat: new solution for 347

--- medium/347. Top K Frequent Elements.go
+++ medium/347. Top K Frequent Elements.go
@@ -67,3 +67,74 @@ func topKFrequent(nums []int, k int) []int {
 //
 //// O(NlogN)
 //// O(N)
+
+type Pair struct {
+	Key   int
+	Count int
+}
+
+func topKFrequent(nums []int, k int) []int {
+	hash := make(map[int]int)
+	for _, val := range nums {
+		hash[val]++
+	}
+
+	counter := make([]Pair, 0, len(hash))
+	for key, count := range hash {
+		counter = append(counter, Pair{
+			Key:   key,
+			Count: count,
+		})
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	quickSelect(counter, 0, len(counter)-1, k-1)
+
+	res := make([]int, 0, k)
+	for i := 0; i < k; i++ {
+		res = append(res, counter[i].Key)
+	}
+	return res
+}
+
+func quickSelect(nums []Pair, start, end int, k int) {
+	if end == start {
+		return
+	}
+
+	randomIndex := start + rand.Intn(end-start+1)
+	pivot := nums[randomIndex]
+	swap(nums, randomIndex, end)
+
+	currentLeft := start
+	for i := start; i <= end; i++ {
+		if nums[i].Count > pivot.Count {
+			swap(nums, i, currentLeft)
+			currentLeft++
+		}
+	}
+
+	swap(nums, end, currentLeft)
+	currentRight := end
+	for i := end; i > currentLeft; i-- {
+		if nums[i].Count < pivot.Count {
+			swap(nums, i, currentRight)
+			currentRight--
+		}
+	}
+
+	if currentLeft <= k && k <= currentRight {
+		return
+	} else if currentLeft > k {
+		quickSelect(nums, start, currentLeft-1, k)
+	} else {
+		quickSelect(nums, currentRight+1, end, k)
+	}
+}
+
+func swap(nums []Pair, i, j int) {
+	nums[i], nums[j] = nums[j], nums[i]
+}
+
+// O(N)
+// O(N)

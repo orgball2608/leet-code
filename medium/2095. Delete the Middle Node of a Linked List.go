b02bf8b9b4ecb28a1a554fc5c8eb7f7262776ccd
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-05-02 21:35:42 +0700

    feat: solve problem 2095

---
+++ medium/2095. Delete the Middle Node of a Linked List.go
@@ -0,0 +1,29 @@
+package medium
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteMiddle(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return nil
+	}
+
+	slow, fast := head, head
+	var prev *ListNode
+	for fast != nil && fast.Next != nil {
+		prev = slow
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	prev.Next = slow.Next
+
+	return head
+}
+
+// Time: O(N)
+// Space: O(1)

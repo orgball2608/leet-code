eb7a5cb1812e66092a693fe27f1d7539ee2907cc
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-05-02 20:05:19 +0700

    feat: solve problem 46

---
+++ medium/46. Permutations.go
@@ -0,0 +1,34 @@
+package medium
+
+func permute(nums []int) [][]int {
+	curSolutions := []int{}
+	ans := [][]int{}
+	used := make([]bool, len(nums))
+	var backtrack func()
+	backtrack = func() {
+		if len(curSolutions) == len(nums) {
+			temp := make([]int, len(nums))
+			copy(temp, curSolutions)
+			ans = append(ans, temp)
+			return
+		}
+
+		for j := range nums {
+			if used[j] {
+				continue
+			}
+			used[j] = true
+			curSolutions = append(curSolutions, nums[j])
+			backtrack()
+			curSolutions = curSolutions[:len(curSolutions)-1]
+			used[j] = false
+		}
+	}
+
+	backtrack()
+
+	return ans
+}
+
+// Time: O(N!)
+// Space: O(N)

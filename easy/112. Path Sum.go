2286330a60e0cbd4b60b177eeecffdbd6841a85e
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-05-07 23:39:13 +0700

    feat: solve problem 112

---
+++ easy/112. Path Sum.go
@@ -0,0 +1,25 @@
+package easy
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left == nil && root.Right == nil {
+		return root.Val == targetSum
+	}
+
+	return hasPathSum(root.Left, targetSum-root.Val) ||
+		hasPathSum(root.Right, targetSum-root.Val)
+}
+
+// Time: O(N)
+// Space: O(H) where H is the height of the tree

f9a21effddfdde8f8561049d2127bbe65f6e5cb5
Huỳnh Minh Quang <quanghuynhwtf@gmail.com>
2025-05-18 22:52:21 +0700

    feat: update 28

--- easy/28. Find the Index of the First Occurrence in a String.go
+++ easy/28. Find the Index of the First Occurrence in a String.go
@@ -1,6 +1,6 @@
 package easy
 
-func strStr(haystack string, needle string) int {
+func strStr1(haystack string, needle string) int {
 	if len(needle) == 0 {
 		return 0
 	}
@@ -17,3 +17,53 @@ func strStr(haystack string, needle string) int {
 	}
 	return -1
 }
+
+// Time: O(N*M)
+// Space: 0(1)
+
+func strStr2(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+
+	base := 256
+	mod := 1_000_000_007
+	m, n := len(needle), len(haystack)
+
+	if m > n {
+		return -1
+	}
+
+	hashNeedle := 0
+	for i := 0; i < m; i++ {
+		hashNeedle = (hashNeedle*base + int(needle[i])) % mod
+	}
+
+	power := 1
+	for i := 0; i < m-1; i++ {
+		power = (power * base) % mod
+	}
+
+	hashHay := 0
+	for i := 0; i < m; i++ {
+		hashHay = (hashHay*base + int(haystack[i])) % mod
+	}
+
+	if hashHay == hashNeedle && haystack[0:m] == needle {
+		return 0
+	}
+
+	for i := m; i < n; i++ {
+		hashHay = (hashHay - int(haystack[i-m])*power%mod + mod) % mod
+		hashHay = (hashHay*base + int(haystack[i])) % mod
+
+		if hashHay == hashNeedle && haystack[i-m+1:i+1] == needle {
+			return i - m + 1
+		}
+	}
+
+	return -1
+}
+
+// Time: O(N)
+// Space: O(1)
